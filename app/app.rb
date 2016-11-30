7a11f3efb837848bb60a0194e62f4a8a083db406
Louisa <louisaspicer@gmail.com>
2016-11-30 11:20:18 +0000

    Adding a bookmark

--- app/app.rb
+++ app/app.rb
@@ -12,6 +12,18 @@ class BookmarkManager < Sinatra::Base
     erb :'links/index'
   end
 
+  get '/links/new' do
+    erb :'links/new_link'
+  end
+
+  post '/links' do
+    Link.create(url: params[:URL], title: params[:title])
+  end
+
+
+
+
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+feature "New links page" do
+  scenario "Adding a bookmark for Google" do
+    visit '/links/new'
+    fill_in('title', with: 'Google')
+    fill_in('URL', with: 'www.google.co.uk')
+    click_button('Submit')
+
+    visit '/links'
+
+    within 'ul#links' do
+      expect(page).to have_content('Google')
+    end
+  end
+end
