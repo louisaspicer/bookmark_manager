44724f508c3a3515f4b5e31e0eda74d523b8ca74
Russell <rusyeo@gmail.com>
2016-11-30 10:38:26 +0000

    /links erb shows links

--- app/app.rb
+++ app/app.rb
@@ -7,9 +7,10 @@ class BookmarkManager < Sinatra::Base
     'Hello BookmarkManager!'
   end
 
-  # get '/links' do
-  #
-  # end
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require './app/models/link'
 
 ENV['RACK_ENV'] = 'test'
 
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require File.join(File.dirname(__FILE__), '..', './app/app.rb')
 
 require 'capybara'
 require 'capybara/rspec'
